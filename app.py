dec91c47e78f4b0d11949288696ae572fa6435be
yllhwa <yll2002mail@gmail.com>
2021-12-06 20:15:23 +0800

    feat: 修改接口直接使用uid查询，鉴权由网关进行

--- app.py
+++ app.py
@@ -17,13 +17,9 @@ def wrap_response(data, code=0, message=""):
     }
     return result
 
-@app.route('/AsoulAnniversary/<token>', methods=["GET"])
-def asoul_anniversary(token):
-    info = verify(token)
-    if info!=None:
-        return jsonify(wrap_response(get_personal_data(info.get("uid")))), 200
-    else:
-        return jsonify(wrap_response(None, code=404, message="授权状态错误")), 200
+@app.route('/AsoulAnniversary/<uid>', methods=["GET"])
+def asoul_anniversary(uid):
+    return jsonify(wrap_response(get_personal_data(uid))), 200
 
 if __name__ == '__main__':
     app.run(host=FLASK_HOST, port=FLASK_PORT)
